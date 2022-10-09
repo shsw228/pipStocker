78d8453868e8e570cef84f22429b53f997cc643b
shsw228 <tate.kng0228@gmail.com>
2022-10-09 15:55:00 +0900

    Initial Commit

---
+++ pipStocker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pipStocker
+//
+//  Created by Kengo Tate on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
